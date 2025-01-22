f9afb498c5edc5d3ca911c48a2ea3ce1396d36f2
akukamil <chamilioner_kid@mail.ru>
2025-01-22 11:55:24 +0300

    123

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 var M_WIDTH=800, M_HEIGHT=450;
-var app, fbs, assets={},client_id, objects={}, state='',chat_path, game_tick=0, made_moves=0, game_id=0, my_turn=0, connected = 1,opponent=0, LANG = 0;
-var hidden=0, game_platform="", hidden_state_start = 0, git_src = '',room_name = 'room0';
-var moving_chip=null, pending_player='',tm={}, some_process = {};
-var my_data={opp_id : ''},opp_data={};
+var app, fbs, assets={},client_id, objects={}, state='',chat_path, game_tick=0, made_moves=0, game_id=0, my_turn=0, connected = 1,opponent=0, LANG = 0, hidden=0, game_platform="", git_src = '',room_name = 'room0', moving_chip=null, pending_player='',tm={}, some_process = {}, my_data={opp_id : ''},opp_data={}, game_name='domino';
+
 const WIN = 1, DRAW = 0, LOSE = -1, NOSYNC = 2;
 
 const shift_w=55;
@@ -3543,16 +3541,6 @@ confirm_dialog = {
 
 keep_alive = function() {
 	
-	if (hidden) {		
-		
-		//убираем из списка если прошло время с момента перехода в скрытое состояние		
-		let cur_ts = Date.now();	
-		let sec_passed = (cur_ts - hidden_state_start)/1000;		
-		if ( sec_passed > 100 )	fbs.ref(room_name+"/"+my_data.uid).remove();
-		return;		
-	}
-
-
 	fbs.ref("players/"+my_data.uid+"/tm").set(firebase.database.ServerValue.TIMESTAMP);
 	fbs.ref("inbox/"+my_data.uid).onDisconnect().remove();
 	fbs.ref(room_name+"/"+my_data.uid).onDisconnect().remove();
@@ -3561,7 +3549,7 @@ keep_alive = function() {
 }
 
 var kill_game = function() {
-	
+	my_ws.kill();
 	firebase.app().delete();
 	document.body.innerHTML = 'CLIENT TURN OFF';
 }
@@ -3914,192 +3902,6 @@ main_menu={
 
 }
 
-my_ws={
-	
-	socket:0,
-	
-	child_added:{},
-	child_changed:{},
-	child_removed:{},
-		
-	get_resolvers:{},
-	get_req_id:0,
-	reconnect_time:5000,
-	connect_resolver:0,
-	sleep:0,
-	keep_alive_timer:0,	
-	keep_alive_time:45000,
-	open_tm:0,
-		
-	init(){		
-		fbs.ref('WSDEBUG/'+my_data.uid).remove();
-		fbs.ref('WSDEBUG/'+my_data.uid).push({tm:Date.now(),event:'init'});
-	
-		if(this.socket.readyState===1) return;
-		return new Promise(resolve=>{
-			this.connect_resolver=resolve;
-			this.reconnect();
-		})
-	},
-	
-	send_to_sleep(){	
-		
-		fbs.ref('WSDEBUG/'+my_data.uid).push({tm:Date.now(),event:'send_to_sleep'});
-		
-		clearTimeout(this.keep_alive_timer);
-		this.sleep=1;	
-		this.socket.close(1000, 'sleep');
-	},
-	
-	kill(){
-		
-		clearTimeout(this.keep_alive_timer);
-		this.sleep=1;
-		this.socket.close(1000, 'kill');
-		
-	},
-	
-	reconnect(){
-				
-		fbs.ref('WSDEBUG/'+my_data.uid).push({tm:Date.now(),event:'reconnect'});
-
-		if (this.socket) {
-			this.socket.onopen = null;
-			this.socket.onmessage = null;
-			this.socket.onclose = null;
-			this.socket.onerror = null;	
-			this.socket.close();
-		}
-				
-		this.open_tm=0;
-		this.sleep=0;		
-		this.socket = new WebSocket('wss://timewebmtgames.ru:8443/domino/'+my_data.uid);
-				
-		this.socket.onopen = () => {
-			console.log('Connected to server!');
-			this.connect_resolver();
-			this.reconnect_time=0;
-			this.open_tm=Date.now();
-			
-			//обновляем подписки
-			for (const path in this.child_added)				
-				this.socket.send(JSON.stringify({cmd:'child_added',path}))					
-			
-			this.reset_keep_alive('onopen');
-		};			
-		
-		this.socket.onmessage = event => {
-			
-			this.reset_keep_alive('onmessage');
-			const msg=JSON.parse(event.data);
-			//console.log("Получено от сервера:", msg);
-			
-			if (msg.event==='child_added')	
-				this.child_added[msg.node]?.(msg);
-			
-			if (msg.event==='get')
-				if (this.get_resolvers[msg.req_id])
-					this.get_resolvers[msg.req_id](msg.data);
-
-		};
-		
-		this.socket.onclose = event => {	
-
-			clearTimeout(this.keep_alive_timer)		
-
-			fbs.ref('WSDEBUG/'+my_data.uid).push({tm:Date.now(),event:'close',code:event.code,reason:event.reason,type:event.type||'no_type'});
-		
-			//не восстанавливаем соединения если закрыто по команде
-			if (['not_alive','no_uid','kill','sleep'].includes(event.reason)) return;
-					
-			if (this.open_tm){
-				const working_time=Date.now()-this.open_tm;
-				this.reconnect_time=10000;
-				if (working_time<this.keep_alive_time)					
-					this.keep_alive_time=Math.max(10000,this.keep_alive_time-5000);					
-			}else{
-				this.reconnect_time=Math.min(60000,Math.floor(this.reconnect_time*1.5));
-			}			
-			
-			console.log(`reconnecting in ${this.reconnect_time*0.001} seconds:`, event);
-			setTimeout(()=>{this.reconnect()},this.reconnect_time);				
-		};
-
-		this.socket.onerror = error => {
-			//fbs.ref('WSERRORS/'+my_data.uid).push({tm:Date.now(),event:'error'});
-		};
-		
-	},
-	
-	reset_keep_alive(reason){
-		console.log('reset_keep_alive',reason)
-		clearInterval(this.keep_alive_timer)
-		this.keep_alive_timer=setTimeout(()=>{
-			
-			try{
-				fbs.ref('WSDEBUG/'+my_data.uid).push({tm:Date.now(),event:'keep_alive'});
-				this.socket.send('1');
-			}catch(e){
-				fbs.ref('WSDEBUG/'+my_data.uid).push({tm:Date.now(),event:'keep_alive_error'});
-			}
-			
-			this.reset_keep_alive('timer');
-			
-		},this.keep_alive_time);
-		
-	},
-	
-	get(path,limit_last){		
-		return new Promise(resolve=>{
-			
-			const req_id=irnd(1,999999);
-						
-			const timeoutId = setTimeout(() => {
-				delete this.get_resolvers[req_id];
-				resolve(0);
-			}, 5000);			
-			
-			this.get_resolvers[req_id]=(data)=>{				
-				clearTimeout(timeoutId);
-				resolve(data);					
-			}
-			
-			/*
-			this.get_resolvers[req_id] = {
-				resolve: (data) => {
-					clearTimeout(timeoutId);
-					resolve(data);
-				}
-			};*/
-			
-			this.socket.send(JSON.stringify({cmd:'get',path,req_id,limit_last}))				
-		
-		})	
-	},
-	
-	ss_child_added(path,callback){
-		
-		this.socket.send(JSON.stringify({cmd:'child_added',path}))	
-		this.child_added[path]=callback;
-		
-	},
-
-	ss_child_changed(path,callback){
-		
-		this.socket.send(JSON.stringify({cmd:'child_changed',node:path}))	
-		this.child_changed[path]=callback;
-		
-	},
-	
-	ss_child_removed(path,callback){
-		
-		this.socket.send(JSON.stringify({cmd:'child_removed',node:path}))	
-		this.child_removed[path]=callback;
-		
-	}	
-		
-}
-
 chat={
 	
 	last_record_end : 0,
@@ -4155,12 +3957,12 @@ chat={
 		await my_ws.init();	
 		
 		//загружаем чат		
-		const chat_data=await my_ws.get('domino/chat',25);
+		const chat_data=await my_ws.get(`${game_name}/chat`,25);
 		
 		await this.chat_load(chat_data);
 		
 		//подписываемся на новые сообщения
-		my_ws.ss_child_added('domino/chat',chat.chat_updated.bind(chat))
+		my_ws.ss_child_added(`${game_name}/chat`,chat.chat_updated.bind(chat))
 		
 		console.log('Чат загружен!')
 	},		
@@ -4439,7 +4241,7 @@ chat={
 		//пишем сообщение в чат и отправляем его		
 		const msg = await keyboard.read(70);		
 		if (msg) {			
-			my_ws.socket.send(JSON.stringify({cmd:'push',path:'domino/chat',val:{uid:my_data.uid,name:my_data.name,msg,tm:'TMS'}}))
+			my_ws.socket.send(JSON.stringify({cmd:'push',path:`${game_name}/chat`,val:{uid:my_data.uid,name:my_data.name,msg,tm:'TMS'}}))
 		}	
 		
 	},
@@ -6393,8 +6195,6 @@ async function init_game_env(lang) {
 	//устанавливаем рейтинг в попап
 	objects.id_rating.text=my_data.rating;
 
-
-
 	//обновляем почтовый ящик
 	fbs.ref('inbox/'+my_data.uid).set({sender:'-',message:'-',tm:'-',data:9});
 
@@ -6423,6 +6223,9 @@ async function init_game_env(lang) {
 	//keep-alive сервис
 	setInterval(function()	{keep_alive()}, 40000);
 
+	//загрузка сокета
+	await auth2.load_script('https://akukamil.github.io/common/my_ws.js');	
+	
 	//ждем загрузки чата
 	await Promise.race([
 		chat.init(),
