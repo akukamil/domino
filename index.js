eb9e40298d473685b8b671dc259af08c2f84d66a
akukamil <chamilioner_kid@mail.ru>
2025-08-05 10:19:34 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1426,7 +1426,7 @@ big_msg={
 		if (opponent===online_player){
 
 			//контрольные концовки логируем на виртуальной машине
-			if (my_data.rating>1900 || opp_data.rating>1900){
+			if (my_data.rating>1990 || opp_data.rating>1990){
 				const duration = Math.floor((Date.now() - opponent.start_time)*0.001);
 				const data={uid:my_data.uid,player1:objects.my_card_name.text,player2:objects.opp_card_name.text, res:result_type,fin_type:result,duration,bg:opponent.blind_game_flag, rating: [old_rating,my_data.rating],game_id,client_id,tm:'TMS'}
 				my_ws.safe_send({cmd:'log',logger:`${game_name}_games`,data});
